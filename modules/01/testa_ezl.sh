1316c100c3aad1ebeece18c3317cab09e0bcb88b
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-11-10 23:05:49 -0300

    E funciona com emoji tanka
    Co-authored-by: Atílio Antônio <AtilioA@users.noreply.github.com>

---
+++ modules/01/testa_ezl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+EXE=./lab01
+IN=in
+OUT=out
+echo Ours is on the left
+for infile in `ls $IN/*.ezl`; do
+    base=$(basename $infile)
+    outfile=$OUT/${base/.ezl/.out}
+    echo $infile
+    $EXE < $infile | diff --ignore-all-space --ignore-matching-lines --side-by-side --width=60 --color=always $outfile -
+    if [[ $? != 0 ]]; then
+        break
+    fi
+done
